5bb0a1f89850d3c70fe18f7b108a09b6f62848db
bradonf333 <bradonf123@gmail.com>
2017-08-23 18:51:53 -0600

    Task interface

---
+++ src/app/tasks/task.ts
@@ -0,0 +1,9 @@
+/** Interface used for a Task  */
+export interface ITask {
+    taskId: number;
+    language: string;
+    hours: number;
+    taskDescription: string;
+    date: string;
+    imageUrl: string;
+}
